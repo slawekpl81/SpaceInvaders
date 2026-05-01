920395a4b93cbd91e46679241d20be61d0df68d8
slawek81pl <slawomir.jona@gmail.com>
2026-05-01 17:15:42 +0200

    add Rectangle struct

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,15 @@
 const rl = @import("raylib");
 
+const Rectangle=struct {
+    x:f32,y:f32,width:f32,height:f32,
+
+    pub fn intersect(self:Rectangle,other:Rectangle)bool{
+        return self.x<other.x + other.width and
+        self.x+self.width>other.x and
+        self.y<other.y+other.height and self.y+self.height > other.y;
+    }
+};
+
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
